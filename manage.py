d964d9dcf237315f415876efa5f8a66e7d5b297f
aaronzh <aaron@burnsred.com.au>
2022-01-06 13:39:38 +1100

    add manage.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from payslip.payslip import command_help, migrate_db, destroy, generate_payslip
+
+
+if __name__ == "__main__":
+    command_help()
+    while True:
+        command = input("\nType your command: ")
+        if command.lower() == 'term':
+            break
+        elif command.lower() == 'help':
+            command_help()
+        elif command.lower() == 'migrate':
+            migrate_db()
+        elif command.lower() == 'destroy':
+            destroy()
+        elif command.split()[0].lower() == 'generatemonthlypayslip':
+            generate_payslip(command)
+        else:
+            print("Your command is invalid, type 'help' to see valid commands")
